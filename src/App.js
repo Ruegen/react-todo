d6126b510394aa3e4e6a1181551404e2a13f945d
ruegen <ruegen@icloud.com>
2019-04-09 15:02:53 +1000

    change app to function type

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from "react"
+import React from "react"
 import logo from "./logo.svg"
 import "./App.css"
 
-class App extends Component {
-  render() {
-    return (
-      <main>
-        <img src={logo} className="brand" alt="brand" />
-        <h1 className="title">React Todo List</h1>
-        <p className="github-link">
-          <a href="https://github.com/Ruegen/react-todo">github repo</a>
-        </p>
-      </main>
-    )
-  }
+const App = () => {
+  return (
+    <main>
+      <img src={logo} className="brand" alt="brand" />
+      <h1 className="title">React Todo List</h1>
+      <p className="github-link">
+        <a href="https://github.com/Ruegen/react-todo">github repo</a>
+      </p>
+    </main>
+  )
 }
 
 export default App
