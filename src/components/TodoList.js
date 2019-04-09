c46eebfb93469ebc08ab0ac8839092ab13d46389
ruegen <ruegen@icloud.com>
2019-04-10 00:22:40 +1000

    add todo list component and tests

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,66 @@
-import React, { hasState, hasEffect } from 'react'
+import React, { useEffect, useReducer } from "react"
+import reducer from './reducers/TodoListReducer'
+import './TodoList.scss'
 
 const TodoList = () => {
-    return <>
-    <form className="todo-form"></form>
-    <ul className="todo-list"></ul>
-    </>
-}
+  const [{todos}, dispatch] = useReducer(reducer, {todos: []})
 
+  useEffect(() => {
+    dispatch({type: 'GET_TODOS'})
+  },[])
+
+  const addTodo = (e) => {
+    e.preventDefault()
+    const title = e.target.title.value
+    if(!title) {
+      return
+    }
+    dispatch({type: 'ADD_TODO', payload: {title}})
+  }
+
+  const updateTodo = (e, id) => {
+    const title = e.target.value
+    dispatch({type: 'UPDATE_TODO', payload: {id, title}})
+  }
+
+  const deleteTodo = (id) => {
+    dispatch({type: 'DELETE_TODO', payload: {id}})
+  }
+
+  const toggleTodoComplete = (id) => {
+    dispatch({type: 'TOGGLE_TODO_COMPLETED', payload: {id}})
+  }
+
+
+  return (
+    <>
+      <form className="todo-form" onSubmit={addTodo}>
+        <input type="text" name="title" />
+      </form>
+      <ul className="todo-list">
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <button
+              onClick={() => toggleTodoComplete(todo.id)}
+              className="button-complete"
+            >
+              <span className={todo.completed ? "completed" : ""} />
+            </button>
+            <input
+              autoComplete="off"
+              className={todo.completed ? "completed" : ""}
+              type="text"
+              name="title"
+              defaultValue={todo.title}
+              onInput={e => updateTodo(e, todo.id)}
+            />
+            <button className="button-delete" onClick={() => deleteTodo(todo.id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
 export default TodoList
--- src/components/TodoList.test.js
+++ src/components/TodoList.test.js
@@ -1,13 +1,19 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import Adapter from 'enzyme-adapter-react-16';
 import TodoList from "./TodoList"
-import { mount, configure } from "enzyme"
+import { mount } from "enzyme"
 
-configure({adapter: new Adapter()})
+const updateInput = (wrapper, instance, val) => {
+  const input = wrapper.find(instance)
+  input.simulate("change", {
+    currentTarget: { value: val }
+  })
+  wrapper.update()
+  return input
+}
 
-describe('Component: TodoList', () => {
-  let wrapper;
+describe("Component: TodoList", () => {
+  let wrapper
   beforeEach(() => {
     wrapper = mount(<TodoList />)
   })
@@ -19,13 +25,31 @@ describe('Component: TodoList', () => {
   })
 
   it("renders form on page", () => {
-      const form = wrapper.find('form.todo-form')
-      expect(form.name()).toBe('form')
+    const form = wrapper.find("form.todo-form")
+    expect(form).toMatchSelector("form")
   })
 
   it("renders list on page", () => {
-      const list = wrapper.find('ul.todo-list')
-      expect(list.name()).toBe('ul')
+    const list = wrapper.find("ul.todo-list")
+    expect(list).toMatchSelector("ul")
+    expect(list).toExist()
   })
 
+  it("renders form title input for new todo", () => {
+    const input = wrapper.find('[name="title"]')
+    expect(input.props().name).toBe("title")
+    expect(input).toExist()
+  })
+
+  it("has an initial state of 0 todos", () => {
+    const todo = wrapper.find("ul.todo-list > li")
+    expect(todo).not.toExist()
+  })
+
+  // const input = updateInput(wrapper, "form > input", "Buy Bread")
+  it("should display a todo", () => {
+    const input = wrapper.find('[name="title"]')
+    input.simulate("change", { target: { value: "Buy Bread" } })
+    expect(input).toHaveValue("Buy Bread")
+  })
 })
