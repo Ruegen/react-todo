dfec8ce12ed2eaa4ad9ea32b07d67f6fb9feb5c2
ruegen <ruegen@icloud.com>
2019-04-09 14:58:32 +1000

    add app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import Adapter from 'enzyme-adapter-react-16';
+import App from "./App"
+import { mount, configure } from "enzyme"
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+configure({adapter: new Adapter()})
+
+describe("Component: AppComponent", () => {
+
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(<App />)
+  })
+
+  it("renders without crashing", () => {
+    const div = document.createElement("div")
+    ReactDOM.render(<App />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+  it(`should have expected title`, () => {
+    const h1 = wrapper.find("h1.title")
+    expect(h1.text()).toBe("React Todo List")
+  })
+
+  it('should have link to github', () => {
+    const link = wrapper.find('p.github-link > a')
+    expect(link.props().href).toBe('https://github.com/Ruegen/react-todo')
+    expect(link.text()).toBe('github repo')
+  })
+
+  
+})
