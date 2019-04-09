166a7696866047ef60e09ca692e12f7776522631
ruegen <ruegen@icloud.com>
2019-04-10 00:23:33 +1000

    remove api from reducer

--- src/components/reducers/TodoListReducer.js
+++ src/components/reducers/TodoListReducer.js
@@ -1,67 +1,82 @@
-import TodoApi from '../todoApi'
 import createTodo from '../todo'
 
-const todoApi = new TodoApi()
+const updateLocalStorage = (todos) => {
+  setTimeout(() => {
+    localStorage.setItem('todos:react', JSON.stringify(todos))
+  }, 300)
+} 
 
 const reducer =  (state, action) => {
   switch (action.type) {
-    case 'GET_TODOS':
-    const todos = todoApi.getTodos()
-    return {
-      ...state,
-      todos
-    }
-    case 'ADD_TODO':
+    case 'GET_TODOS': {
     try {
+      const todos = JSON.parse(localStorage.getItem('todos:react'))
+      if(todos) {
+        return {
+          ...state,
+          todos
+        }
+      }
+      return state
+    } catch(err) {
+      console.error(err)
+      return state
+    }
+    }
+    
+    case 'ADD_TODO': {
+
       const {title} = action.payload
       const todo = createTodo(title)
-      todoApi.addTodo(todo)
+      const todos = [todo, ...state.todos]
+      updateLocalStorage(todos)
       return {
         ...state,
-        todos: [todo, ...state.todos]
-      }
-    } catch(err) {
-      console.log(err) 
-      return {
-        ...state
+        todos
       }
     }
-    case 'DELETE_TODO':
-    const {id} = action.payload
-    try {
-      todoApi.deleteTodoById(id)
-      return {...state,
-        todos: state.todos.filter(t => t.id !== id)
-      }
-    } catch(err) {
-      return state
-    }
-    case 'UPDATE_TODO':
-    try {
-      const {id, title} = action.payload
-      todoApi.updateTodo(id, title)
-      const todo = state.todos.find(t => t.id === id)
-      todo.title = title
-      return {
-        ...state,
-        todos: state.todos.map(t => t.id === id ? todo : t)
-      } 
-    } catch (err) {
-      return state
-    }
-    case 'TOGGLE_TODO_COMPLETED':
-    try {
+    
+    case 'DELETE_TODO': {
+
       const {id} = action.payload
-      todoApi.toggleTodoComplete(id)
-      const todo = state.todos.find(t => t.id === id)
-      todo.completed = !todo.completed
+        const todos = state.todos.filter(t => t.id !== id)
+        updateLocalStorage(todos)
+        return {...state,
+          todos
+        }
+    }
+        
+    case 'UPDATE_TODO': {
+      const {id, title} = action.payload
+      const todos = state.todos.map(t => {
+        if(t.id === id) {
+          t.title = title
+          return t
+        }
+        return t
+      })
+      updateLocalStorage(todos)
       return {
         ...state,
-        todos: state.todos.map(t => t.id === id ? todo : t)
+        todos
       } 
-    } catch (err) {
-      return state
     }
+    case 'TOGGLE_TODO_COMPLETED': {
+      const {id} = action.payload;
+      const todos = state.todos.map(t => {
+        if(t.id === id) {
+          t.completed = !t.completed
+          return t
+        }
+        return t
+      })
+      updateLocalStorage(todos)
+      return {
+        ...state,
+        todos
+      }
+    }
+    
     default: 
     return state
   }
