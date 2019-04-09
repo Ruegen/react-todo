65fe6f55a8114634203bf047c71dba36823802da
ruegen <ruegen@icloud.com>
2019-04-10 00:22:48 +1000

    add app css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import logo from "./logo.svg"
 import "./App.css"
+import TodoList from './components/TodoList'
 
 const App = () => {
   return (
     <main>
       <img src={logo} className="brand" alt="brand" />
       <h1 className="title">React Todo List</h1>
+      <TodoList/>
       <p className="github-link">
         <a href="https://github.com/Ruegen/react-todo">github repo</a>
       </p>
